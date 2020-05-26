7f1a93edf5b20bbbb2587dffe217221a7e08ac3a
AFunChild <13112230086@163.com>
2020-05-26 18:15:31 +0800

    android x bg color

--- lsh_ble_app/src/main/java/com/hnnk/lsh/ble/MainActivity.java
+++ lsh_ble_app/src/main/java/com/hnnk/lsh/ble/MainActivity.java
@@ -2,8 +2,11 @@ package com.hnnk.lsh.ble;
 
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.feng.mydemo.activity.BleScanActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +14,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        startActivity(new Intent(this, BleScanActivity.class));
+
+        Intent dialogIntent = new Intent(getBaseContext(), BleScanActivity.class);
+        dialogIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        getApplication().startActivity(dialogIntent);
     }
 }
