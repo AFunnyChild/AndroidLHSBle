467370698ee282b5737546a6d4cd58145f1acde8
AFunChild <13112230086@163.com>
2020-06-22 19:29:37 +0800

    ble bug fix

--- lsh_ble_app/src/main/java/com/hnnk/lsh/ble/LHSBleMainActivity.java
+++ lsh_ble_app/src/main/java/com/hnnk/lsh/ble/LHSBleMainActivity.java
@@ -2,30 +2,20 @@ package com.hnnk.lsh.ble;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.graphics.drawable.AnimationDrawable;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.os.CountDownTimer;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.FrameLayout;
-import android.widget.ImageView;
-import android.widget.Toast;
-import com.feng.mydemo.activity.BleScanActivity;
-import com.tbruyelle.rxpermissions2.RxPermissions;
-import com.zhihu.matisse.Matisse;
-import com.zhihu.matisse.MimeType;
-import com.zhihu.matisse.engine.impl.PicassoEngine;
-import com.zhihu.matisse.filter.Filter;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    static MainActivity  activity;
+import com.feng.mydemo.activity.BleScanActivity;
+
+
+public class LHSBleMainActivity extends AppCompatActivity implements View.OnClickListener {
+    static LHSBleMainActivity activity;
     private static final int REQUEST_CODE_CHOOSE = 23;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,11 +50,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == REQUEST_CODE_CHOOSE && resultCode == RESULT_OK) {
-            //  Matisse.obtainResult(data)+
-            // mAdapter.setData(Matisse.obtainResult(data), Matisse.obtainPathResult(data));
-            Log.e("OnActivityResult state", String.valueOf(Matisse.obtainOriginalState(data)));
-            Log.e("OnActivityResult ",Matisse.obtainPathResult(data).get(0).toString());
-        //    onSelectPhotoPath(Matisse.obtainPathResult(data).get(0).toString());
+
         }
     }
 
@@ -78,30 +64,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     public static void JniSelectPhoto() {
-        activity.select_photo();
+
     }
 
-    public void select_photo() {
-        RxPermissions rxPermissions = new RxPermissions(this);
-        rxPermissions.request(Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                .subscribe(aBoolean -> {
-                    if (aBoolean) {
-                            Matisse.from(MainActivity.this)
-                                    .choose(MimeType.ofImage())
-                                    .theme(R.style.Matisse_Dracula)
-                                    .countable(false)
-                                    .addFilter(new GifSizeFilter(320, 320, 5 * Filter.K * Filter.K))
-                                    .maxSelectable(1)
-                                    .originalEnable(true)
-                                    .maxOriginalSize(10)
-                                    .imageEngine(new PicassoEngine())
-                                    .forResult(REQUEST_CODE_CHOOSE);
 
-                    } else {
-                        Toast.makeText(MainActivity.this, "无权限", Toast.LENGTH_LONG).show();
-                    }
-                }, Throwable::printStackTrace);
-    }
 
  //   public static native void   onSelectPhotoPath(String path);
 }
