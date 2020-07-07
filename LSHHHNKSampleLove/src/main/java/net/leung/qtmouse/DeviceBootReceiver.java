b1915cdb5639dc16651f3c6d1554317b06ed86c3
AFunChild <13112230086@163.com>
2020-07-07 09:20:12 +0800

    testlib

--- LSHHHNKSampleLove/src/main/java/net/leung/qtmouse/DeviceBootReceiver.java
+++ LSHHHNKSampleLove/src/main/java/net/leung/qtmouse/DeviceBootReceiver.java
@@ -52,7 +52,7 @@ public class DeviceBootReceiver extends BroadcastReceiver {
     private void MainPageGo(Context context) {
         Class<?> activity_class=null;
         try {
-            activity_class   = Class.forName("net.leung.qtmouse1.MainActivity");
+            activity_class   = Class.forName("net.leung.qtmouse.MainActivity");
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
