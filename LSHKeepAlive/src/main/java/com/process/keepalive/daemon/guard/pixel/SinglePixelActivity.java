c53c49937aa2427d9d1138a45777777487623a0f
LiuSongHan <121513057@qq.com>
2020-07-17 16:03:51 +0800

    test lib

--- LSHKeepAlive/src/main/java/com/process/keepalive/daemon/guard/pixel/SinglePixelActivity.java
+++ LSHKeepAlive/src/main/java/com/process/keepalive/daemon/guard/pixel/SinglePixelActivity.java
@@ -1,6 +1,9 @@
 package com.process.keepalive.daemon.guard.pixel;
 
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,9 +13,11 @@ import android.view.Window;
 import android.view.WindowManager;
 
 import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
 
 import com.process.keepalive.daemon.guard.DaemonEnv;
 import com.process.keepalive.daemon.guard.LogUtils;
+import com.process.keepalive.daemon.guard.R;
 import com.process.keepalive.daemon.guard.WatchDogService;
 
 public class SinglePixelActivity extends Activity {
@@ -40,6 +45,7 @@ public class SinglePixelActivity extends Activity {
         params.height = 1;
         window.setAttributes(params);
 
+        LogUtils.i("Keep","SinglePixelActivity onCreate");
         ScreenManager.getInstance().setKeepLiveActivity(this);
     }
 
